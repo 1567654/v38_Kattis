c8a2e0cfb1dbcb5be077e96263226538c1caf241
1567654 <luohuanyu3124@gmail.com>
2018-09-21 15:20:31 +0200

    Alla 3 kattis uppgifter

---
+++ Kattis uppgifter/src/kattis/R2.java
@@ -0,0 +1,24 @@
+package kattis;
+
+import java.util.Scanner;
+
+public class R2 {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        
+        int R1 = input.nextInt();   
+        
+        int S = input.nextInt();
+        
+        int R2 = (2 * S - R1);
+        
+        System.out.println(R2);
+        
+        input.close();
+        
+    }
+
+}
---
+++ Kattis uppgifter/src/kattis/Stuck_in_a_timeloop.java
@@ -0,0 +1,22 @@
+package kattis;
+
+import java.util.Scanner;
+
+public class Stuck_in_a_timeloop {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int N = input.nextInt();
+		
+		for (int i = 1; i <= N; i++) {
+			System.out.println( i + " Abracadabra");
+			
+		}
+		
+		
+				
+	}
+
+}
